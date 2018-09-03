dccafffabfcbdff5cd23afc8e17f9849c6d2dfd9
root <root@Phenomenal.localdomain>
2018-09-03 20:20:11 +0500

    contact.Js container added

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -6,11 +6,15 @@ class Contacts extends Component {
 
     return (
       <div className="card card-body mb-3">
-        <h4 >{name} </h4>
+      <div className="container">
+      <h4 >{name} </h4>
         <ul className="list-group">
           <li className="list-group-item">Email: {email}</li>
           <li className="list-group-item">Phone: {phone}</li>
         </ul>
+
+      </div>
+        
       </div>
     );
   }
