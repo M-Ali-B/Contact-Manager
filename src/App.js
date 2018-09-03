b54b2d9a836fce08b5b8706d1596302006047e26
root <root@Phenomenal.localdomain>
2018-09-03 15:05:14 +0500

    App.js modified

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ class App extends Component {
         <Header />
       <Contacts name="Ali" email="ali@gmail.com" phone="0321-xxx-xxxx"/>
       <Contacts name="Bacha" email="bacha@gmail.com" phone="0322-xxx-xxxx"/>
+      <Contacts name="Hassan" email="hassan@gmail.com" phone="0323-xxx-xxxx"/>
+      <Contacts name="Faisal" email="faisal@gmail.com" phone="0323-xxx-xxxx"/>
 </div>
     );
   }
