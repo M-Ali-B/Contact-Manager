e5fe519bb9e17be44a3c7251fae365d7593d7662
root <root@Phenomenal.localdomain>
2018-09-03 20:13:40 +0500

    Bootstrap added

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -5,11 +5,11 @@ class Contacts extends Component {
     const { name, email, phone } = this.props;
 
     return (
-      <div>
-        <h4>{name} </h4>
-        <ul>
-          <li>Email: {email}</li>
-          <li>Phone: {phone}</li>
+      <div className="card card-body mb-3">
+        <h4 >{name} </h4>
+        <ul className="list-group">
+          <li className="list-group-item">Email: {email}</li>
+          <li className="list-group-item">Phone: {phone}</li>
         </ul>
       </div>
     );
