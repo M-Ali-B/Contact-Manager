325a206691d7b86411e020de4b7a70e8fe1be253
root <root@Phenomenal.localdomain>
2018-09-16 22:32:39 +0500

    State with NO Constructor

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,48 +1,57 @@
-import React, {Component} from 'react'
+import React, {
+    Component
+} from 'react'
 import Contact from './Contact'
 class Contacts extends Component {
-   constructor(){
-       super();
-       this.state={  // values of key of contacts
-           contacts:[
-{
-    id: 1,
-    name: 'John Doe',
-    email:'jdon@gmail.com',
-    phone:'555-555-5555'
-},
-{
-    id: 2,
-    name: 'Henry Doe',
-    email: 'hdon@gmail.com',
-    phone: '333-333-3333'
-},
-{
-    id: 3,
-    name: 'Jesse Doe',
-    email: 'jessdon@gmail.com',
-    phone: '222-222-2222'
-}
+    state = { // values of key of contacts
+        contacts: [{
+                id: 1,
+                name: 'John Doe',
+                email: 'jdon@gmail.com',
+                phone: '555-555-5555'
+            },
+            {
+                id: 2,
+                name: 'Henry Doe',
+                email: 'hdon@gmail.com',
+                phone: '333-333-3333'
+            },
+            {
+                id: 3,
+                name: 'Jesse Doe',
+                email: 'jessdon@gmail.com',
+                phone: '222-222-2222'
+            }
 
-           ]
-       };
-   }
-   
-   
-   
-    render(){
-        const {contacts} = this.state;
-        return ( 
-<div>
-{contacts.map(contact=>(
-    <Contact  
-    key = {contact.id}
-    name={contact.name} 
-    email = {contact.email}
-    phone = {contact.phone}
-    />
-))}
-</div>
+        ]
+    };
+
+
+
+
+    render() {
+        const {
+            contacts
+        } = this.state;
+        return ( <
+            div > {
+                contacts.map(contact => ( <
+                    Contact key = {
+                        contact.id
+                    }
+                    name = {
+                        contact.name
+                    }
+                    email = {
+                        contact.email
+                    }
+                    phone = {
+                        contact.phone
+                    }
+                    />
+                ))
+            } <
+            /div>
 
         )
     }
