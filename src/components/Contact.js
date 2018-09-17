47b1b4802317bf20f96ffdbf20a8dccbb21716c0
root <root@Phenomenal.localdomain>
2018-09-17 20:29:16 +0500

    just chilling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,37 +1,64 @@
-import React, { Component } from "react";
+import React, {
+  Component
+} from "react";
 
 class Contact extends Component {
-  constructor(){
-    super();
-    this.state={};
+  state = {
+    showContactInfo: true
 
-    this.onShowClick = this.onShowClick.bind(this);
+  };
+
+  // this.onShowClick = this.onShowClick.bind(this);
+
+
+
+onShowClick = e => {
+
+  this.setState({
+    showContactInfo: !this.state.showContactInfo
+  });
+  // console.log(this.state);
+};
+
+render() {
+  const {
+    name,
+    email,
+    phone
+  } = this.props; //destructuring 
+
+  return ( 
     
-  }
+    <div className = "card card-body mb-3" >
+    
+    <div className = "container" >
+    
+    <h4 > {
+      name
+    } < i onClick = {
+      this.onShowClick
+    }
+    className = "fas fa-sort-down" > < /i></h4 >
+    <
+    ul className = "list-group" >
+    <
+    li className = "list-group-item" > Email: {
+      email
+    } < /li> <
+    li className = "list-group-item" > Phone: {
+      phone
+    } < /li> <
+    /ul>
 
-  onShowClick(){
+    <
+    /div>
 
-    console.log(this.state);
-  }
-  
-  render() {
-    const { name, email, phone } = this.props; //destructuring 
-
-    return (
-      <div className="card card-body mb-3">
-      <div className="container">
-      <h4 >{name} <i  onClick={this.onShowClick} className="fas fa-sort-down"></i></h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-        </ul>
-
-      </div>
-        
-      </div>
-    );
-  }
+    <
+    /div>
+  );
 }
+};
+
 
 
 
